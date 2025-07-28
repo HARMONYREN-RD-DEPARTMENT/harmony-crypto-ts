90d5b10a91bfc71d1257e87d463363ec039a897d
Kai5041 <kai5041contact@gmail.com>
2025-07-28 13:49:14 +0200

    Added Dilithium5 and Kyber1024 support for signing and encapsulation

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,1 +1,22 @@
-export declare function hello(): void;
+import { MlKem1024 } from "mlkem";
+declare class HarmonyCrypto {
+    dilithium: any;
+    mlkem: MlKem1024;
+    seed: Uint8Array;
+    private constructor();
+    static create(seed?: Uint8Array): Promise<HarmonyCrypto>;
+    derive_seed(info: string, length: number): Uint8Array;
+    get_dilithium5_keypair(): {
+        publicKey: Uint8Array;
+        privateKey: Uint8Array;
+    };
+    get_kyber1024_keypair(): Promise<{
+        publicKey: Uint8Array;
+        privateKey: Uint8Array;
+    }>;
+    sign(message: Uint8Array, privateKey: Uint8Array): Uint8Array;
+    verify(signature: Uint8Array, message: Uint8Array, publicKey: Uint8Array): boolean;
+    kyber1024_encapsulate(publicKey: Uint8Array): Promise<[Uint8Array, Uint8Array]>;
+    kyber1024_decapsulate(ciphertext: Uint8Array, shared_secret: Uint8Array): Promise<Uint8Array>;
+}
+export default HarmonyCrypto;
--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,50 @@
-export function hello() {
-    console.log("Hello from harmony-crypto-ts");
+import Dilithium from "dilithium-crystals-js";
+import { sha3_256 } from '@noble/hashes/sha3';
+import { hkdf } from '@noble/hashes/hkdf';
+import { MlKem1024 } from "mlkem";
+class HarmonyCrypto {
+    constructor(dilithium, mlkem, seed) {
+        this.dilithium = dilithium;
+        this.mlkem = mlkem;
+        this.seed = seed;
+    }
+    static async create(seed) {
+        if (!seed) {
+            seed = crypto.getRandomValues(new Uint8Array(64));
+        }
+        const dilithium = await Dilithium;
+        const mlkem = new MlKem1024();
+        return new HarmonyCrypto(dilithium, mlkem, seed);
+    }
+    derive_seed(info, length) {
+        return hkdf(sha3_256, this.seed, new Uint8Array(0), new TextEncoder().encode(info), length);
+    }
+    get_dilithium5_keypair() {
+        const { publicKey, privateKey } = this.dilithium.generateKeys(2, this.derive_seed("dilithium5 signing keypair", 32));
+        if (!privateKey || !publicKey) {
+            throw new Error("Dilthium5 private key or public key is undefined");
+        }
+        return { publicKey, privateKey };
+    }
+    async get_kyber1024_keypair() {
+        const [publicKey, privateKey] = await this.mlkem.deriveKeyPair(this.derive_seed("kyber1024 encrypting keypair", 64));
+        if (!privateKey || !publicKey) {
+            throw new Error("Kyber1024 private key or public key is undefined");
+        }
+        return { publicKey, privateKey };
+    }
+    sign(message, privateKey) {
+        return this.dilithium.sign(message, privateKey, 2).signature;
+    }
+    verify(signature, message, publicKey) {
+        console.log(signature.length);
+        return this.dilithium.verify(signature, message, publicKey, 2).result === 0;
+    }
+    kyber1024_encapsulate(publicKey) {
+        return this.mlkem.encap(publicKey);
+    }
+    kyber1024_decapsulate(ciphertext, shared_secret) {
+        return this.mlkem.decap(ciphertext, shared_secret);
+    }
 }
+export default HarmonyCrypto;
